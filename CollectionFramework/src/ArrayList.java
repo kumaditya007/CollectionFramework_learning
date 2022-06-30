7bef7f79b9b6b1b3ecdd09e763d17230f5bcce50
Aditya kumar <kumaditya007@gmail.com>
2022-06-30 23:40:03 +0530

    CF 1.1 commit

---
+++ CollectionFramework/src/ArrayList.java
@@ -0,0 +1,13 @@
+/****************************************************************************
+ * 
+ * @author kumaditya007@gmail.com, sample demos for ArrayList
+ * 
+ *****************************************************************************/
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+	}
+
+}
